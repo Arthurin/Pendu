a597aae978932bc7ff39b6a59b486dc6a934d0f1
Arthurin <6535135+Arthurin@users.noreply.github.com>
2025-06-25 16:30:51 +0200

    jeu terminé, codé moi-même il marche mais il manque l'ajout de nouveaux mots

--- App.jsx
+++ App.jsx
@@ -4,8 +4,16 @@ import Key from "./Key";
 export default function Pendu() {
   const [answer, setAnswer] = React.useState("pomme");
   const [guessedLetters, setGuessedLetters] = React.useState(new Set());
-  console.log("refresh");
-  console.log(guessedLetters);
+
+  const answerSet = new Set(answer);
+  const wrongLetters = guessedLetters.difference(answerSet);
+  const correctLetters = guessedLetters.intersection(answerSet);
+  console.log("Mauvaises lettres :");
+  console.log(wrongLetters);
+  const endGame = gameEnded();
+  const missingLetters = endGame
+    ? answerSet.difference(guessedLetters)
+    : new Set();
   /*
   function addLetter(letter) {
     console.log("addLetter " + letter);
@@ -25,18 +33,56 @@ export default function Pendu() {
     });
   }
 
-  let answerLettersElements = answer
-    .split("")
-    .map((letter, index) => (
-      <span key={index}>
-        {guessedLetters.has(letter) ? letter.toUpperCase() : "."}
-      </span>
-    ));
+  function isWon() {
+    return answerSet.isSubsetOf(guessedLetters);
+  }
+
+  // défaite à partir de la 7ème tentative
+  function isLost() {
+    console.log(wrongLetters.size > 6);
+    return wrongLetters.size > 6;
+  }
+
+  function gameEnded() {
+    console.log("game ended? " + (isWon() || isLost()));
+    return isWon() || isLost();
+  }
+
+  function newGame() {
+    setGuessedLetters(new Set());
+  }
+
+  let answerLettersElements = answer.split("").map((letter, index) => (
+    <span
+      key={index}
+      style={{
+        color: missingLetters.has(letter) ? "rgb(216, 89, 89)" : "white",
+      }}
+    >
+      {guessedLetters.has(letter)
+        ? letter.toUpperCase()
+        : isLost()
+        ? letter.toUpperCase()
+        : ""}
+    </span>
+  ));
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   let keyboardElements = alphabet
     .split("")
     .map((letter, index) => (
-      <Key key={index} myLetter={letter} clickMethod={addLetter} />
+      <Key
+        key={index}
+        myLetter={letter}
+        endGame={endGame}
+        letterColor={
+          wrongLetters.has(letter)
+            ? "rgb(255, 76, 44)"
+            : correctLetters.has(letter)
+            ? "rgb(38, 174, 81)"
+            : "rgb(255, 188, 44)"
+        }
+        clickMethod={addLetter}
+      />
     ));
 
   return (
@@ -45,20 +91,29 @@ export default function Pendu() {
         <h1>Jeu du pendu</h1>
         <p>Devine le mot en moins de 8 essais !</p>
 
-        <section className="game-status">
-          <h2>Tu as gagné !</h2>
-          <p>Bravo ! 🎉🥳🎉</p>
-        </section>
-
         <section>
-          <img alt="Nb d'erreur : 0" src="/0.jpg" />
+          <img
+            alt={`Nb d'erreur : ${wrongLetters.size}`}
+            src={`/${wrongLetters.size}.jpg`}
+          />
         </section>
 
         <section className="answer">{answerLettersElements}</section>
 
+        {isWon() ? (
+          <section className="game-status">
+            <h2>Tu as gagné !</h2>
+            <p>Bravo ! 🎉🥳🎉</p>
+          </section>
+        ) : (
+          ""
+        )}
+
         <section className="keyboard">{keyboardElements}</section>
 
-        <button className="newGame">Nouvelle partie</button>
+        <button className="newGame" onClick={newGame}>
+          Nouvelle partie
+        </button>
       </header>
     </main>
   );
--- Key.jsx
+++ Key.jsx
@@ -1,7 +1,14 @@
 export default function Key(props) {
+  const styles = {
+    backgroundColor: props.letterColor,
+  };
   function clickKey() {
     console.log("click");
     props.clickMethod(props.myLetter);
   }
-  return <button onClick={clickKey}>{props.myLetter.toUpperCase()}</button>;
+  return (
+    <button disabled={props.endGame} onClick={clickKey} style={styles}>
+      {props.myLetter.toUpperCase()}
+    </button>
+  );
 }
